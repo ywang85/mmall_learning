ecffbe2a92baf8d7d1703060bbc7e9e25d773335
WangYiJie <wangyijie@datagrand.com>
2020-02-23 11:49:40 +0800

    商品管理模块

--- src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
